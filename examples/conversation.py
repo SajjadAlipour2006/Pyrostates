3d15f6c58b8d65a7b1211a7387c711e54692b3e2
SajjadAlipour2006 <136921317+SajjadAlipour2006@users.noreply.github.com>
2024-06-07 17:26:44 +0330

    Added `conversation.py` example

---
+++ examples/conversation.py
@@ -0,0 +1,27 @@
+from pyrogram import Client, filters
+from pyrostates import at_state, set_state, del_state
+
+app = Client("my_account")
+
+
+@app.on_message(at_state(None) & filters.private & filters.text)
+async def home_state(client, message):
+    await message.reply("Hello, I'm the conversation bot\nWhat is your name?")
+    set_state(message, "NAME")
+
+
+@app.on_message(at_state("NAME") & filters.private & filters.text)
+async def name_state(client, message):
+    name = message.text
+    await message.reply(f"Nice to meet you, {name}!\nHow old are you?")
+    set_state(message, "AGE")
+
+
+@app.on_message(at_state("AGE") & filters.private & filters.text)
+async def age_state(client, message):
+    age = message.text
+    await message.reply(f"You are {age} years old, good for you!\nHave a nice day!")
+    del_state(message)
+
+
+app.run()
