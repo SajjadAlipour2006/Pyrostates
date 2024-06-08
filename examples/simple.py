388236cc456bd1e7766bcea8f59863416454c269
SajjadAlipour2006 <136921317+SajjadAlipour2006@users.noreply.github.com>
2024-06-09 02:26:52 +0330

    Refactored some code

--- examples/simple.py
+++ examples/simple.py
@@ -5,20 +5,20 @@ app = Client("my_account")
 
 
 @app.on_message(at_state(None) & filters.private & filters.text)
-async def home_state(client, message):
+async def home_state(_, message):
     await message.reply("Hello, I'm the conversation bot\nWhat is your name?")
     set_state(message, "NAME")
 
 
 @app.on_message(at_state("NAME") & filters.private & filters.text)
-async def name_state(client, message):
+async def name_state(_, message):
     name = message.text
     await message.reply(f"Nice to meet you, {name}!\nHow old are you?")
     set_state(message, "AGE")
 
 
 @app.on_message(at_state("AGE") & filters.private & filters.text)
-async def age_state(client, message):
+async def age_state(_, message):
     age = message.text
     await message.reply(f"You are {age} years old, good for you!\nHave a nice day!")
     del_state(message)
