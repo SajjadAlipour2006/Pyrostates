929cff825a42a4a45a9277502e485d78fdad46ee
SajjadAlipour2006 <136921317+SajjadAlipour2006@users.noreply.github.com>
2024-06-12 04:04:07 +0330

    Fixed issue in the advanced example

--- examples/advanced.py
+++ examples/advanced.py
@@ -1,5 +1,5 @@
 from pyrogram import Client, filters
-from pyrostates import StateMachine, StateGroup, State, set_state, del_state
+from pyrostates import StateMachine, StateGroup, State
 
 app = Client("my_account")
 
@@ -15,21 +15,21 @@ state_machine = StateMachine("user_states.db", States)
 @app.on_message(state_machine.at(None) & filters.private & filters.text)
 async def home_state(_, message):
     await message.reply("Hello, I'm the conversation bot\nWhat is your name?")
-    set_state(message, "NAME")
+    state_machine[message] = "NAME"
 
 
 @app.on_message(state_machine.at(States.NAME) & filters.private & filters.text)
 async def name_state(_, message):
     name = message.text
     await message.reply(f"Nice to meet you, {name}!\nHow old are you?")
-    set_state(message, "AGE")
+    state_machine[message] = "AGE"
 
 
 @app.on_message(state_machine.at(States.AGE) & filters.private & filters.text)
 async def age_state(_, message):
     age = message.text
     await message.reply(f"You are {age} years old, good for you!\nHave a nice day!")
-    del_state(message)
+    del state_machine[message]
 
 
 app.run()
