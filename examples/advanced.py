3865f30ff8f2ad1ae0df3f1de433819902d6d2f5
SajjadAlipour2006 <136921317+SajjadAlipour2006@users.noreply.github.com>
2024-06-13 15:37:10 +0330

    Refactored some code

--- examples/advanced.py
+++ examples/advanced.py
@@ -14,21 +14,21 @@ state_machine = StateMachine("user_states.db", States)
 
 @app.on_message(state_machine.at(None) & filters.private & filters.text)
 async def home_state(_, message):
-    await message.reply("Hello, I'm the conversation bot\nWhat is your name?")
+    await message.reply("Hello, I'm the conversation bot. What is your name?")
     state_machine[message] = States.NAME
 
 
 @app.on_message(state_machine.at(States.NAME) & filters.private & filters.text)
 async def name_state(_, message):
     name = message.text
-    await message.reply(f"Nice to meet you, {name}!\nHow old are you?")
+    await message.reply(f"Nice to meet you, {name}! How old are you?")
     state_machine[message] = States.AGE
 
 
 @app.on_message(state_machine.at(States.AGE) & filters.private & filters.text)
 async def age_state(_, message):
     age = message.text
-    await message.reply(f"You are {age} years old, good for you!\nHave a nice day!")
+    await message.reply(f"You are {age} years old, good for you! Have a nice day!")
     del state_machine[message]
 
 
--- examples/simple.py
+++ examples/simple.py
@@ -6,21 +6,21 @@ app = Client("my_account")
 
 @app.on_message(at_state(None) & filters.private & filters.text)
 async def home_state(_, message):
-    await message.reply("Hello, I'm the conversation bot\nWhat is your name?")
+    await message.reply("Hello, I'm the conversation bot. What is your name?")
     set_state(message, "NAME")
 
 
 @app.on_message(at_state("NAME") & filters.private & filters.text)
 async def name_state(_, message):
     name = message.text
-    await message.reply(f"Nice to meet you, {name}!\nHow old are you?")
+    await message.reply(f"Nice to meet you, {name}! How old are you?")
     set_state(message, "AGE")
 
 
 @app.on_message(at_state("AGE") & filters.private & filters.text)
 async def age_state(_, message):
     age = message.text
-    await message.reply(f"You are {age} years old, good for you!\nHave a nice day!")
+    await message.reply(f"You are {age} years old, good for you! Have a nice day!")
     del_state(message)
 
 
