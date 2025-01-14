6d67a9934eb118a139f9831c7d69811b2b8e253f
SajjadAlipour2006 <136921317+SajjadAlipour2006@users.noreply.github.com>
2025-01-14 06:19:56 +0330

    Pyrostates 1.0.1

--- pyrostates/__init__.py
+++ pyrostates/__init__.py
@@ -1,6 +1,6 @@
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 
 from .state_group import StateGroup
 from .state import State
 from .state_machine import StateMachine
-from .global_state_machine import get_state, set_state, del_state, AtState as at_state
+from .global_state_machine import get_state, set_state, del_state, del_state_if_exists, AtState as at_state
